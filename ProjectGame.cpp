252fa9699b11c9d7813cf4f19859d4437dc13c29
Lazar <LPApostolov21@codingburgas.bg>
2022-12-07 21:49:36 +0200

    Deleting the upper code

--- ProjectGame.cpp
+++ ProjectGame.cpp
@@ -4,64 +4,66 @@
 using namespace std;
 void game()
 {
-	char sentance[100], temp = 0;
-	cin.getline(sentance, 100, '\n');
-	bool counter = false, mistakescounter = false;
-	int mistakes = 0, arr[100];
-	for (size_t i = 0; i < strlen(sentance); i++)
-	{
-		if ((sentance[i] < 65 || sentance[i]>90) && (sentance[i] > 122 || sentance[i] < 97))
-		{
-			arr[i] = 3;
-		}
-		else
-		{
-			arr[i] = 2;
-		}
-	}
-	while (counter != true)
-	{
-		counter = true;
-		for (size_t i = 0; i < strlen(sentance); i++)
-		{
-			if (((sentance[i] == temp || sentance[i] + 32 == temp || sentance[i] - 32 == temp) && ((temp >= 65 && temp <= 90) || (temp >= 97 && temp <= 122))) || arr[i] != 2)
-			{
-				if (arr[i] == 2)
-				{
-					arr[i] = 1;
-					mistakescounter = false;
-				}
-				cout << sentance[i];
-			}
-			else
-			{
-				cout << "_";
-				counter = false;
-			}
-		}
-		if (mistakescounter == true)
-		{
-			mistakes++;
-		}
-		cout << "  " << mistakes << "/5 MISTAKES";
-		cout << '\n';
-		//LOSE CONDITION
-		if (mistakes >= 5)
-		{
-			cout << "YOU LOSE!";
-			break;
-		}
-		//WIN CONDITION
-		if (counter == true)
-		{
-			cout << "YOU WIN!";
-			break;
-		}
-		cin >> temp;
-		mistakescounter = true;
-	}
+    char sentance[100], temp = 0;
+    cin.getline(sentance, 100, '\n');
+    bool counter = false, mistakescounter = false;
+    int mistakes = 0, arr[100];
+    for (size_t i = 0; i < strlen(sentance); i++)
+    {
+        if ((sentance[i] < 65  sentance[i]>90) && (sentance[i] > 122  sentance[i] < 97))
+        {
+            arr[i] = 3;
+        }
+        else
+        {
+            arr[i] = 2;
+        }
+    }
+    system("cls");
+    while (counter != true)
+    {
+        counter = true;
+        for (sizet i = 0; i < strlen(sentance); i++)
+        {
+            if (((sentance[i] == temp  sentance[i] + 32 == temp  sentance[i] - 32 == temp) && ((temp >= 65 && temp <= 90)  (temp >= 97 && temp <= 122)))  arr[i] != 2)
+            {
+                if (arr[i] == 2)
+                {
+                    arr[i] = 1;
+                    mistakescounter = false;
+                }
+                cout << sentance[i];
+            }
+            else
+            {
+                cout << "";
+                counter = false;
+            }
+        }
+        if (mistakescounter == true)
+        {
+            mistakes++;
+        }
+        cout << "  " << mistakes << "/5 MISTAKES";
+        cout << '\n';
+        //LOSE CONDITION
+        if (mistakes >= 5)
+        {
+            cout << "YOU LOSE!";
+            break;
+        }
+        //WIN CONDITION
+        if (counter == true)
+        {
+            cout << "YOU WIN!";
+            break;
+        }
+        cin >> temp;
+        mistakescounter = true;
+        system("cls");
+    }
 }
 int main()
 {
-	game();
+    game();
 }
